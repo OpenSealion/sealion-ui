2d2fcb035c43b721086f0ce41b447aa8cfa91d4a
杜晓卿 <duxiaoqing@mininglamp.com>
2023-03-22 13:45:32 +0800

    更新欢饮页&新增icon button

--- src/components/core/sl-button/sl-button.tsx
+++ src/components/core/sl-button/sl-button.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import classNames from 'classnames';
 
-export type SLButtonTypes = 'primary' | 'secondary' | 'secondary2' | 'line' | 'text';
+export type SLButtonTypes = 'primary' | 'secondary' | 'secondary2' | 'line' | 'text' | 'icon' | 'icon2';
 export type SLButtonSizes = 'normal' | 'large' | 'small';
 export type SLHostButtonType = 'button' | 'submit' | 'reset';
 export type SLButtonStatus = 'success' | 'warn' | 'danger';
 
-export interface SLButtonProps {
+export interface SLButtonProps extends Omit<React.ButtonHTMLAttributes<any>, 'type'> {
     btnType?: SLButtonTypes;
     size?: SLButtonSizes;
     className?: string;
@@ -15,8 +15,8 @@ export interface SLButtonProps {
 }
 
 const SLButton: React.FC<SLButtonProps> = ({
-    btnType = 'primary',
-    size = 'normal',
+    btnType,
+    size,
     className,
     children,
     type = 'button',
