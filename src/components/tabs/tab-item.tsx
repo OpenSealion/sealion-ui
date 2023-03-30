278a94ba2a696b0a1476f012b230438c02737662
杜晓卿 <duxiaoqing@mininglamp.com>
2023-03-30 15:53:00 +0800

    feat: tabs

--- src/components/tabs/tab-item.tsx
+++ src/components/tabs/tab-item.tsx
@@ -1,15 +1,19 @@
 import React, { useRef } from 'react';
 import Button from '../button';
 
-
 const TabItem = ({
     children
 }) => {
-    const ref = useRef(null);
+    const itemRef = useRef(null);
 
     return (
-
+        <div
+            className="seal-tab-item"
+            ref={itemRef}
+        >
+            <Button>{children}</Button>
+        </div>
     );
-}
+};
 
 export default TabItem;
