d2a7d29f413d0f0ab2883dc07cd55f6d4d874649
杜晓卿 <duxiaoqing@mininglamp.com>
2023-03-30 11:34:51 +0800

    feat: tabs

---
+++ src/components/tabs/tab-item.tsx
@@ -0,0 +1,15 @@
+import React, { useRef } from 'react';
+import Button from '../button';
+
+
+const TabItem = ({
+    children
+}) => {
+    const ref = useRef(null);
+
+    return (
+
+    );
+}
+
+export default TabItem;
