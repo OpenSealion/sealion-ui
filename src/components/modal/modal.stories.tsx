d1f161e4e6c2edd20dcc895893fe75c75306cd60
xiaomin <xiaomin@pjlab.org.cn>
2023-05-22 13:22:38 +0800

    openxlab-sealion feat: add modal demos

--- src/components/modal/modal.stories.tsx
+++ src/components/modal/modal.stories.tsx
@@ -2,6 +2,9 @@ import Modal, {IModalProps} from "./modal";
 import {ComponentMeta, ComponentStory} from "@storybook/react";
 import Button from "../button/button";
 import React from "react";
+import ModalCard from "./modal-card";
+import IconFont from '../icon';
+import message from "../message/index";
 export default ({
     title: 'Modal',
     component: Modal
@@ -38,6 +41,71 @@ defaultModal.args = {
     cancelText: '取消',
     children: '这是一段常规描述性文字',
 };
+
+export const typesModal = () => {
+    return (
+        <div style={{ display: 'flex', flexWrap: 'wrap', gap: 36 }}>
+            <ModalCard
+                open
+                onClose={() => null}
+                title={'这是一个普通的弹窗'}
+            >
+                这是一段常规描述性文字
+            </ModalCard>
+            <ModalCard
+                open
+                onClose={() => null}
+                title={'可以没有关闭按钮'}
+                closeable={false}
+            >
+                这是一段常规描述性文字
+            </ModalCard>
+            <ModalCard
+                open
+                onClose={() => null}
+                title={'可以没有遮罩层'}
+                maskClosable={false}
+                closeable={false}
+            >
+                这是一段常规描述性文字
+            </ModalCard>
+            <ModalCard
+                open
+                onClose={() => null}
+                title={'可以修改icon'}
+                closeable={false}
+                icon={<IconFont icon={'icon-CheckCircleFilled'} style={{ color: '#00B365', fontSize: 24 }} />}
+            >
+                这是一段常规描述性文字
+            </ModalCard>
+            <ModalCard
+                open
+                onClose={() => null}
+                title={'可以修改按钮文字'}
+                closeable={false}
+                icon={<IconFont icon={'icon-InfoFilled'} style={{ color: '#faad14', fontSize: 24 }} />}
+                okText={'知道了'}
+                cancelText={'算了吧'}
+            >
+                这是一段常规描述性文字
+            </ModalCard>
+            <ModalCard
+                open
+                onClose={() => null}
+                title={'可以添加关闭回调'}
+                closeable={false}
+                okText={'点击执行回调'}
+                onOk={() => {
+                    message.success({title: '这是一个成功消息通知'});
+                }}
+            >
+                这是一段常规描述性文字
+            </ModalCard>
+        </div>
+    )
+};
+
+typesModal.storyName = '不同类型的弹窗';
 
 export const normalModal = () => {
     const [open, setOpen] = React.useState(false);
--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -4,6 +4,7 @@ import { CSSTransition } from 'react-transition-group';
 import IconFont from '../icon';
 import Button from '../button/button';
 import { ReactPortal } from '../react-portal';
+import ModalCard from './modal-card';
 
 export interface IModalProps {
     children?: React.ReactNode; // 用于自定义内容
@@ -47,46 +48,6 @@ const Modal: React.FC<IModalProps> = (props) => {
     const nodeRef = React.useRef(null);
 
     const wrapperClasses = classNames('seal-modal-wrapper', className);
-    const Icon = icon || <IconFont icon="icon-InfoFilled" style={{ color: '#1B67FF', fontSize: 24 }} />;
-    const CloseIcon = closeIcon || (
-        <Button btnType="icon" onClick={onClose}>
-            <IconFont icon="icon-cuowu1" style={{ fontSize: 24, cursor: 'pointer' }} />
-        </Button>
-    );
-
-    const FooterNode = footer || (
-        <div className="seal-modal-footer">
-            <Button
-                btnType="secondary"
-                onClick={() => {
-                    onCancel();
-                    onClose();
-                }}
-            >
-                {cancelText || '取消'}
-            </Button>
-            <Button
-                btnType="primary"
-                onClick={() => {
-                    onOk();
-                    onClose();
-                }}
-            >
-                {okText || '确定'}
-            </Button>
-        </div>
-    );
-
-    useEffect(() => {
-        // 使用useEffect来监听键盘事件，esc键关闭弹窗
-        const closeOnEscapeKey = e => (e.key === 'Escape' ? onClose() : null);
-        document.body.addEventListener('keydown', closeOnEscapeKey);
-        return () => {
-            document.body.removeEventListener('keydown', closeOnEscapeKey);
-        };
-    }, [onClose]);
-
-    // if (!open) return null;
 
     return (
         <ReactPortal>
@@ -99,17 +60,7 @@ const Modal: React.FC<IModalProps> = (props) => {
             >
                 <div className="seal-modal-root" ref={nodeRef}>
                     {maskClosable && <div className="seal-modal-mask" onClick={onClose} />}
-                    <div className={wrapperClasses} style={{ width, ...style }}>
-                        {showIcon && Icon}
-                        <div className="seal-modal-right">
-                            <div className="seal-modal-header">
-                                <div className="seal-modal-title">{title}</div>
-                                {closeable && CloseIcon}
-                            </div>
-                            <div className="seal-modal-content">{children}</div>
-                            {FooterNode}
-                        </div>
-                    </div>
+                    <ModalCard {...props} className={wrapperClasses} />
                 </div>
             </CSSTransition>
         </ReactPortal>
