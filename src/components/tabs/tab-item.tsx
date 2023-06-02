ad235ebe295cdb4c5dacc2da22f6979200679593
duxiaoqing <duxiaoqing@pjlab.org.cn>
2023-06-02 15:53:46 +0800

    feat:tabs

--- src/components/tabs/tab-item.tsx
+++ src/components/tabs/tab-item.tsx
@@ -1,17 +1,77 @@
-import React, { useRef } from 'react';
-import Button from '../button';
+import React, { useRef, useEffect } from 'react';
+import classNames from 'classnames';
 
-const TabItem = ({
-    children
+export type ItemKeyType = number | string;
+
+export interface TabItemInfoProps {
+    width: number;
+    left: number;
+    itemKey: ItemKeyType;
+}
+export interface TabItemProps {
+    label: string;
+    itemKey: ItemKeyType;
+    type?: 'text' | 'card' | 'line'
+    style?: React.CSSProperties;
+    className?: string;
+    active: boolean;
+    disabled?: boolean;
+    onClick: (key: TabItemInfoProps) => void;
+    onMounted: (info: TabItemInfoProps) => void;
+}
+
+const TabItem: React.FC<TabItemProps> = ({
+    children,
+    itemKey,
+    type = 'text',
+    style,
+    className,
+    active,
+    disabled,
+    onClick,
+    onMounted,
+    ...rest
 }) => {
     const itemRef = useRef(null);
+    const classes = classNames(
+        className,
+        'seal-tab-item',
+        active && 'seal-tab-item-active',
+        disabled && 'seal-tab-item-active',
+        `seal-tab-${type}`
+    );
+
+    const handleClick = () => {
+        const width = itemRef.current.clientWidth;
+        const left = itemRef.current.offsetLeft;
+        onClick({
+            width,
+            left,
+            itemKey
+        });
+    };
+
+    useEffect(() => {
+        if (itemRef.current && itemKey !== undefined && typeof onMounted === 'function') {
+            const width = itemRef.current.clientWidth;
+            const left = itemRef.current.offsetLeft;
+            onMounted({
+                width,
+                left,
+                itemKey
+            });
+        }
+    }, [itemKey]);
 
     return (
         <div
-            className="seal-tab-item"
+            className={classes}
             ref={itemRef}
+            style={style}
+            onClick={handleClick}
+            {...rest}
         >
-            <Button>{children}</Button>
+            {children}
         </div>
     );
 };
